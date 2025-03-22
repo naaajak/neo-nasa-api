297696c733dc7697e0048c4de26d8c74e9dfd1cd
Karol Radecki <radeckikarol7@gmail.com>
2025-03-22 10:31:27 +0100

    Etap 3

--- spark-apps/test_spark.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder \
-    .appName("Test Spark") \
-    .getOrCreate()
-
-# Sprawdź dane w pamięci Sparka
-df = spark.sql("SELECT * FROM neo_data")
-df.show()
